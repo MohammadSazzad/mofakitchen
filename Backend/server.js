a559594383ee00567475d9ef8bc02d736ff1ad40
MohammadSazzad <sazzad19@student.sust.edu>
2024-12-21 21:43:44 +0600

    xzz

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import userRouter from './route/users.js';
+import userPreferenceRouter from './route/userPreference.js';
+import recipeRouter from './route/recipe.js';
+
+dotenv.config();
 
 const app = express();
+
+app.use('/api/users', userRouter);
+app.use('/api/userPreference', userPreferenceRouter);
+app.use('/api/recipe', recipeRouter);
+
+const port = process.env.SERVER_PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
     }
 );
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
     }
 );
 
